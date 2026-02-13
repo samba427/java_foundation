973eeb1be39882765bc98c0b4b534b7935005411
samba427 <samriddhbag@gmail.com>
2026-02-13 23:47:41 +0530

    added variable demo in basics

---
+++ src/basics/variabledemo.java
@@ -0,0 +1,17 @@
+package basics;
+
+public class variabledemo {
+    public static void main(String[] args) {
+        int age = 19;
+        double height = 5.9;
+        char grade = 'A';
+        boolean isStudent = true;
+        String name = "Sam";
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + height);
+        System.out.println("Grade: " + grade);
+        System.out.println("Student: " + isStudent);
+    }
+}
