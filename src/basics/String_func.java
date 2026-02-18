60cee9c1dd7d90b3841282250b311bdd40588ad0
samba427 <samriddhbag@gmail.com>
2026-02-19 02:44:03 +0530

    learnt upto nested loops

---
+++ src/basics/String_func.java
@@ -0,0 +1,15 @@
+package basics;
+
+public class String_func
+{
+    void main(String[] args)
+    {
+        String name="Samba";
+        int l=name.length();
+        // also have charAt, indexOf, lastIndexOf, toUpperCase
+        // replace to change sm chars... isEmpty to check if empty, trim removes spaces
+        // contains will check if sm is contained
+        // equals and equalsIgnoreCase
+        // substring(start,end) creates a substring from that start index to that end index
+    }
+}
