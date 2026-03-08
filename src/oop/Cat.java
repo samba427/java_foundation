15d47b313ddb6e73730b4fb8f06cd35850e2f08a
samba427 <samriddhbag@gmail.com>
2026-03-08 22:45:12 +0530

    Learn abt overriding methods... add the term @Override for better readability

--- src/oop/Cat.java
+++ src/oop/Cat.java
@@ -1,6 +1,7 @@
 package oop;
 
 public class Cat extends Animal{
+    @Override //good prct to write this when overriding for other devs to know
     void Eats()
     {
         System.out.println("Cat eats mice");
